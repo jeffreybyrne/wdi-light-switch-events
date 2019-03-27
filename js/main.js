37d4258f1552abeb17469d4f905945da12cc5930
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-27 14:17:08 -0400

    finished the exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  const lightSwitch = document.getElementsByClassName('switch')[0];
+  const statusText = document.querySelector('.status');
+  const body = document.querySelector('body');
+  lightSwitch.addEventListener('click', function() {
+    lightSwitch.classList.toggle('on');
+    lightSwitch.classList.toggle('off');
+    body.classList.toggle('light');
+    body.classList.toggle('dark');
+    if (statusText.innerText == "It's so bright in here!") {
+      statusText.innerText = "Hey, who turned off the lights?"
+    } else {
+      statusText.innerText = "It's so bright in here!"
+    }
+  })
+});
